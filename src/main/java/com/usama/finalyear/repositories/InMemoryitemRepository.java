36741e7ba1de72a7ee8f4ea746b467cfc582c6ca
usama <usamakhalil562@gmail.com>
2019-08-21 15:16:59 -0700

    test save method

--- src/main/java/com/usama/finalyear/repositories/InMemoryitemRepository.java
+++ src/main/java/com/usama/finalyear/repositories/InMemoryitemRepository.java
@@ -1,3 +1,4 @@
+
 package com.usama.finalyear.repositories;
 
 import com.usama.finalyear.domain.Item;
@@ -7,20 +8,30 @@ import java.util.List;
 import java.util.Optional;
 
 public class InMemoryitemRepository implements ItemRepositories {
+    static int n = 1;
+
     private List<Item> list = new ArrayList<Item>();
 
     public Item save(Item item) {
+       //  for (int n=1;n<=list.size();n++) {
 
-        list.add(item);
+
+             item.setId(n);
+             list.add(item);
+             n = n + 1;
+       //  }
         return item;
+
     }
-    public List<Item> findAll(){
+
+    public List<Item> findAll() {
         return list;
     }
+
     public Optional<Item> findById(long id) {
         for (int i = 0; i < list.size(); i++) {
-            Item  item = list.get(i);
-            if (id == item.getId() ) {
+            Item item = list.get(i);
+            if (id == item.getId()) {
                 return Optional.of(item);
             }
         }
--- src/test/java/com/usama/finalyear/repositories/InMemoryItemRepositoryTest.java
+++ src/test/java/com/usama/finalyear/repositories/InMemoryItemRepositoryTest.java
@@ -1,13 +1,20 @@
 package com.usama.finalyear.repositories;
 
 import com.usama.finalyear.domain.Item;
-import com.usama.finalyear.repositories.InMemoryitemRepository;
+import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.List;
 
 public class InMemoryItemRepositoryTest {
+
+    @After
+    public void resetStatic(){
+        InMemoryitemRepository.n=1;
+    }
+
     @Test
     public void canretriveNewItem(){
         Item item = new Item ();
@@ -16,7 +23,36 @@ public class InMemoryItemRepositoryTest {
 
         List<Item> list = itemrepository.findAll();
         assertEquals(1,list.size());
+    }
 
+    @Test
+    public void saveItemShouldHaveId1(){
+        Item item = new Item ();
+        InMemoryitemRepository itemrepository = new InMemoryitemRepository ();
+        Item savedItem = itemrepository.save(item);
+        assertEquals(1,savedItem.getId());
+    }
+
+    @Test
+    public void saveItemShouldHaveIds(){
+        Item item1 = new Item ();
+        Item item2 = new Item ();
+
+        InMemoryitemRepository itemrepository = new InMemoryitemRepository ();
+        Item savedItem1 = itemrepository.save(item1);
+        assertEquals(1,savedItem1.getId());
+
+        Item savedItem2 = itemrepository.save(item2);
+        assertEquals(2,savedItem2.getId());
+    }
+    @Test
+    @Ignore
+    public void createItemAndSetItsIdAndGet(){
+        Item item = new Item();
+        InMemoryitemRepository itemrepository = new InMemoryitemRepository();
+        item.setId(10);
+        Item savedItem = itemrepository.save(item);
+        assertEquals(10,savedItem.getId());
     }
 }
 
